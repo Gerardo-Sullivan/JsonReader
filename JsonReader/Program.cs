83a043d4cbf8ee60085a08795269e732290db1ba
Gerardo Sullivan <em12664@aut.ac.nz>
2019-03-14 15:03:11 +1300

    Add CreateAutBuildingsJson()

--- JsonReader/Program.cs
+++ JsonReader/Program.cs
@@ -21,9 +21,60 @@ namespace JsonReader
         private static readonly string _kiaOra = @"C:\Users\Gerardo\Downloads\KB\JSON\KiaOra.json";
         private static readonly string _library = @"C:\Users\Gerardo\Downloads\KB\JSON\Library.json";
         private static readonly string _orientation = @"C:\Users\Gerardo\Downloads\KB\JSON\Orientation.json";
-
+        private static readonly string _autBuildings = @"C:\Users\em12664\Documents\TUA\Building.Code.json";
+        private static readonly string _utterances = "Location";
 
         public static void Main(string[] args)
+        {
+            CreateAutBuildingsJson();
+        }
+
+        private static void CreateAutBuildingsJson()
+        {
+            List<EntityValues> entityValues = new List<EntityValues>();
+            List<Utterance> utterances = new List<Utterance>();
+            var autBuildings = JsonConvert.DeserializeObject<List<AUTBuilding>>(File.ReadAllText(_autBuildings));
+
+            foreach (AUTBuilding building in autBuildings)
+            {
+                entityValues.Add(new EntityValues(building.Code, building.Code, building.Name, building.BrandingName));
+                if (!string.IsNullOrWhiteSpace(building.Code))
+                {
+                    utterances.Add(new Utterance("Where is the", building.Code, _utterances, " building", EntityType.BuildingCode));
+                    utterances.Add(new Utterance("Where is", building.Code, _utterances, entityType: EntityType.BuildingCode));
+                }
+                if (!string.IsNullOrWhiteSpace(building.Name))
+                {
+                    utterances.Add(new Utterance("Where is the", building.Name, _utterances, entityType: EntityType.BuildingCode));
+                    utterances.Add(new Utterance("Where is", building.Name, _utterances, entityType: EntityType.BuildingCode));
+                    if (building.Name.Contains("&"))
+                    {
+                        string buldingName = building.Name.Replace("&", "and");
+                        utterances.Add(new Utterance("Where is the", buldingName, _utterances, entityType: EntityType.BuildingCode));
+                        utterances.Add(new Utterance("Where is", buldingName, _utterances, entityType: EntityType.BuildingCode));
+                    }
+                    if (building.Name.Contains("+"))
+                    {
+                        string buldingName = building.Name.Replace("+", "and");
+                        utterances.Add(new Utterance("Where is the", buldingName, _utterances, entityType: EntityType.BuildingCode));
+                        utterances.Add(new Utterance("Where is", buldingName, _utterances, entityType: EntityType.BuildingCode));
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(building.BrandingName))
+                {
+                    utterances.Add(new Utterance("Where is the", building.BrandingName, _utterances, " building", EntityType.BuildingCode));
+                    utterances.Add(new Utterance("Where is", building.BrandingName, _utterances, entityType: EntityType.BuildingCode));
+                }
+            }
+
+            var utterancesFirst99 = utterances.Take(99).ToList();
+            var utterancesSkip99 = utterances.Skip(99).ToList();
+            File.WriteAllText("TuaLuisFirst99Utterances.json", JsonConvert.SerializeObject(utterancesFirst99, Formatting.Indented));
+            File.WriteAllText("TuaLuisSkip99Utterances.json", JsonConvert.SerializeObject(utterancesSkip99, Formatting.Indented));
+            File.WriteAllText("TUA_LUIS_BuildingCode.json", JsonConvert.SerializeObject(entityValues, Formatting.Indented));
+        }
+
+        private static void CreateLuisJson()
         {
             LuisApplication application = new LuisApplication();
 
