2590d493828a098fb62b8b053156cd51d09b2e3a
Gerardo Sullivan <em12664@aut.ac.nz>
2019-03-14 15:02:31 +1300

    Add json classed for luis entities and the Location intent

---
+++ JsonReader/AUTBuilding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace JsonReader
+{
+    [Serializable]
+    public class AUTBuilding
+    {
+        [JsonProperty("Building_Id")]
+        public int BuildingId { get; set; }
+
+        [JsonProperty("Campus_Id")]
+        public int CampusId { get; set; }
+
+        [JsonProperty("Code")]
+        public string Code { get; set; }
+
+        [JsonProperty("Address")]
+        public string Address { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Branding_Name")]
+        public string BrandingName { get; set; }
+
+        [JsonProperty("Other_Details")]
+        public string OtherDetails { get; set; }
+    }
+}
---
+++ JsonReader/LuisEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace JsonReader
+{
+    [Serializable]
+    public class EntityValues
+    {
+        [JsonProperty("canonicalForm")]
+        public string CanonicalForm { get; set; }
+
+        [JsonProperty("list")]
+        public List<string> List { get; set; }
+
+        public EntityValues(string canonicalForm, params string[] list)
+        {
+            CanonicalForm = canonicalForm;
+            List = new List<string>();
+            foreach (string item in list)
+            {
+                if (!string.IsNullOrWhiteSpace(item))
+                {
+                    List.Add(item);
+                }
+            }
+        }
+    }
+}
